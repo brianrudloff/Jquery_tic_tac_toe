276d009ff53e06afd1f517b05d804296dc0daff4
Brian Rudloff <brianrudloff@gmail.com>
2017-02-24 18:00:15 -0800

    eslint update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,6 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+    "extends": "airbnb-base",
+    "plugins": [
+        "import"
+    ]
 };
